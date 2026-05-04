4b905ffc3abdf37ec506ad8761112c53848f9e5d
jparkinson97 <jamesparkinson1997@outlook.com>
2026-05-04 19:55:48 +0100

    fix tesseract install

--- hooks/hook_tesseract.py
+++ hooks/hook_tesseract.py
@@ -6,7 +6,8 @@ import pytesseract
 
 base = getattr(sys, "_MEIPASS", os.path.dirname(sys.executable))
 
-tesseract_bin = os.path.join(base, "tesseract", "tesseract")
+_tess_exe = "tesseract.exe" if sys.platform == "win32" else "tesseract"
+tesseract_bin = os.path.join(base, "tesseract", _tess_exe)
 tessdata_dir  = os.path.join(base, "tesseract", "tessdata")
 
 if os.path.isfile(tesseract_bin):
